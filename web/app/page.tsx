2dedfed3c9b68f5fcc53d0ee79b3f4f30fbdee19
bcd <634344724@qq.com>
2024-04-14 00:20:28 +0800

    init

---
+++ web/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from 'antd';
+export default function Home() {
+  return (
+      <div>
+        <div>left</div>
+        <div>middle</div>
+        <div>right</div>
+      </div>
+  );
+}
