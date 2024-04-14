634b4a0b35c987ec1ab6cd5fa28afccbb756f374
bcd <634344724@qq.com>
2024-04-14 13:58:03 +0800

    style change

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { useState } from "react";
 import { Button, Input, Radio } from "antd";
+import { TokenBlock } from "@/components/tokenBlock";
 
 import "./style.css";
-
 export default function Home() {
-  const [link, setLink] = useState("");
   const [pool, setPool] = useState("");
-  const [peopleNum, setPeopleNum] = useState(0);
+  const [peopleNum, setPeopleNum] = useState('');
+  const [tokenList, setTokenList] = useState(["sol", "doge", "sui"]);
+  const [optList, setOptList] = useState(["购买", "取出"]);
+  const [currentToken, setCurrentToken] = useState("");
+  const [currentOpt, setCurrentOpt] = useState("");
 
-  const onChangeLink = (val: string) => {
-    setLink(val);
+  const onChangePeople = (val: string) => {
+    setPeopleNum(val);
   };
 
   const onChangePool = (val: string) => {
@@ -18,58 +21,71 @@ export default function Home() {
   };
 
   return (
-    <div className="p-4">
+    <div className="ai-container">
+      <div className="top-left-block"></div>
       <div className="text-right">
         <div className="inline-block">bot余额: 200ETH</div>
         <Button className="mx-5">存款</Button>
         <Button className="mx-5">取款</Button>
         <Button className="mx-5">链接钱包</Button>
       </div>
-      <div className="flex mt-10">
-        <div className="left-style">left</div>
+      <div className="link-opt-container">
         <div className="middle-style">
           <div className="dynamic-operation">
-            <div>链: 在{link}上</div>
+            <div>你是一个专业的区块链交易员</div>
+            <div>链: 在{currentToken}上</div>
             <div>
-              Token: 池子{pool} 人数{peopleNum}
+              Token: 池子{pool} 参与人数{peopleNum}
+            </div>
+            <div>
+              操作方式:{currentOpt}
+            </div>
+            <div>
+              现在开始工作吧
+            </div>
+            <div>
+              具体怎么工作程序员会知道的
+            </div>
+            <div>
+              是的....
+            </div>
+            <div>
+              你只需要等待指令就好
             </div>
           </div>
         </div>
         <div className="right-style">
           <div>
-            {/* <Icon type="link" /> */}
             <div className="link-title-style">链</div>
-            <div>
-              <Radio.Group
-                buttonStyle="solid"
-                onChange={(val) => {
-                  onChangeLink(val.target.value);
-                }}
-              >
-                <Radio.Button value="BTC">BTC</Radio.Button>
-                <Radio.Button value="ETH">ETH</Radio.Button>
-                <Radio.Button value="SOL">SOL</Radio.Button>
-                <Radio.Button value="Doge">Doge</Radio.Button>
-              </Radio.Group>
+            <div className="token-list">
+              {tokenList.map((o) => (
+                <TokenBlock
+                  token={o}
+                  isSelected={currentToken === o}
+                  onClick={(val) => setCurrentToken(o)}
+                />
+              ))}
             </div>
-            <div style={{ fontSize: 20 }} className="pb-8 pt-16">
-              Token
-            </div>
+            <div className="token-title-style">Token</div>
             <div className="mb-4">
               池子大小:
-              <Input style={{ width: 100, marginLeft: 12 }} />
+              <Input style={{ width: 100, marginLeft: 12 }} onChange={(val) => onChangePool(val.target.value)}/>
             </div>
             <div>
               参与人数:
-              <Input style={{ width: 100, marginLeft: 12 }} />
+              <Input style={{ width: 100, marginLeft: 12 }} onChange={(val) => onChangePeople(val.target.value)} />
             </div>
-            <div style={{ fontSize: 20 }} className="pb-8 pt-16">
-              操作
+            <div className="opt-title-style">操作</div>
+            <div className="token-list">
+              {optList.map((o) => (
+                <TokenBlock
+                  token={o}
+                  isSelected={currentOpt === o}
+                  onClick={(val) => setCurrentOpt(o)}
+                />
+              ))}
             </div>
-            <div className="mb-4">
-              <Button>购买</Button>
-              <Button className="ml-4">出售</Button>
-            </div>
+            <div className="operation-style">执行</div>
           </div>
         </div>
       </div>
