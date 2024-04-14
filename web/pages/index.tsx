0ba8a91a5051af34724ad0397543840923fce419
aLIEzsss4 <vuetomato@gmail.com>
2024-04-14 15:01:22 +0800

    update: _app

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,12 +2,12 @@
 import { useState } from "react";
 import { Button, Input, Radio } from "antd";
 import { TokenBlock } from "@/components/tokenBlock";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-import "./style.css";
 export default function Home() {
   const [pool, setPool] = useState("");
-  const [peopleNum, setPeopleNum] = useState('');
-  const [tokenList, setTokenList] = useState(["sol", "doge", "sui"]);
+  const [peopleNum, setPeopleNum] = useState("");
+  const [tokenList, setTokenList] = useState(["op", "arb", "polygon"]);
   const [optList, setOptList] = useState(["购买", "取出"]);
   const [currentToken, setCurrentToken] = useState("");
   const [currentOpt, setCurrentOpt] = useState("");
@@ -23,11 +23,13 @@ export default function Home() {
   return (
     <div className="ai-container">
       <div className="top-left-block"></div>
-      <div className="text-right">
+      <div className="text-right flex justify-end items-center">
         <div className="inline-block">bot余额: 200ETH</div>
         <Button className="mx-5">存款</Button>
         <Button className="mx-5">取款</Button>
-        <Button className="mx-5">链接钱包</Button>
+        <div className="mr-0 flex">
+          <ConnectButton />
+        </div>
       </div>
       <div className="link-opt-container">
         <div className="middle-style">
@@ -37,21 +39,11 @@ export default function Home() {
             <div>
               Token: 池子{pool} 参与人数{peopleNum}
             </div>
-            <div>
-              操作方式:{currentOpt}
-            </div>
-            <div>
-              现在开始工作吧
-            </div>
-            <div>
-              具体怎么工作程序员会知道的
-            </div>
-            <div>
-              是的....
-            </div>
-            <div>
-              你只需要等待指令就好
-            </div>
+            <div>操作方式:{currentOpt}</div>
+            <div>现在开始工作吧</div>
+            <div>具体怎么工作程序员会知道的</div>
+            <div>是的....</div>
+            <div>你只需要等待指令就好</div>
           </div>
         </div>
         <div className="right-style">
@@ -70,11 +62,17 @@ export default function Home() {
             <div className="token-title-style">Token</div>
             <div className="mb-4">
               池子大小:
-              <Input style={{ width: 100, marginLeft: 12 }} onChange={(val) => onChangePool(val.target.value)}/>
+              <Input
+                style={{ width: 100, marginLeft: 12 }}
+                onChange={(val) => onChangePool(val.target.value)}
+              />
             </div>
             <div>
               参与人数:
-              <Input style={{ width: 100, marginLeft: 12 }} onChange={(val) => onChangePeople(val.target.value)} />
+              <Input
+                style={{ width: 100, marginLeft: 12 }}
+                onChange={(val) => onChangePeople(val.target.value)}
+              />
             </div>
             <div className="opt-title-style">操作</div>
             <div className="token-list">
